89818fb103c7bc065b44c0f6c14a248c22b89cc9
raulgp <raul.garrigos@hotmail.com>
2023-11-04 17:20:40 +0100

    Lab done except styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+// GET "/movies" => Listar todas las películas
+router.get("/movies", async (req, res, next) => {
+  try {
+    const response = await Movie.find().select({ title: 1, image: 1 });
+    res.render("movies.hbs", {
+      allMovies: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET "movies/:id" => Para dar los detalles de las películas
+
+router.get("/movies/:movieId", async (req, res, next) => {
+  try {
+    const response = await Movie.findById(req.params.movieId);
+    res.render("details.hbs", {
+      oneMovie: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
